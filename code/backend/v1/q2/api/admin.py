f5763955780a5976a7c4081bd5891b6a860b49fb
Sebastian Otto <git-1@emmrich-otto.de>
2021-03-23 21:51:15 +0100

    Add columns to site admin table

--- code/backend/v1/q2/api/admin.py
+++ code/backend/v1/q2/api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Link
 
-admin.site.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'reference', 'created_at', 'last_modified_at']
+
+admin.site.register(Link, LinkAdmin)
